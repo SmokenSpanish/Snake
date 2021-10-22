3053aea3bb5214116a4d7c45fd0e3a29d859a588
Maksim Basov <maksimbasov1992@mail.ru>
2021-10-22 12:39:15 +0300

    feat: make rules of the end game

--- scripts.js
+++ scripts.js
@@ -69,6 +69,7 @@ function createMouse() {
 createMouse();
 
 let direction = 'right';
+let steps = false;
 
 function move() {
     snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
@@ -79,54 +80,71 @@ function move() {
     if (direction == "right") {
         if (snakeCoordinates[0] < 10) {
             snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-         } else {
+        } else {
             snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'))
         }
     } else if (direction == "left") {
         if (snakeCoordinates[0] > 1) {
             snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-         } else {
+        } else {
             snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'))
         }
     } else if (direction == "up") {
         if (snakeCoordinates[1] < 10) {
-            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1]+1) + '"]'));
-         } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] + 1) + '"]'));
+        } else {
             snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
         }
     } else if (direction == "down") {
         if (snakeCoordinates[1] > 1) {
-            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1]-1) + '"]'));
-         } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1] - 1) + '"]'));
+        } else {
             snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'));
         }
     }
 
     if (snakeBody[0].getAttribute('posX') == mouse.getAttribute('posX') && snakeBody[0]
-    .getAttribute('posY') == mouse.getAttribute('posY')) {
+        .getAttribute('posY') == mouse.getAttribute('posY')) {
         mouse.classList.remove('mouse');
-        let a = snakeBody[snakeBody.length-1].getAttribute('posX');
-        let b = snakeBody[snakeBody.length-1].getAttribute('posY');
+        let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
         snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
         createMouse();
+    }
+
+    if (snakeBody[0].classList.contains('snake__body')) {
+        setTimeout(() => {
+            alert('GAME OVER');
+        }, 300);
+
+        clearInterval(interval);
+        snakeBody[0].classList.add('snake__dead');
     }
 
     snakeBody[0].classList.add('snake__head');
     for (let i = 0; i < snakeBody.length; i++) {
         snakeBody[i].classList.add('snake__body');
     }
+
+    steps = true;
 }
 
 let interval = setInterval(move, 300);
 
 window.addEventListener('keydown', function (e) {
-    if (e.key == 'ArrowLeft' && direction != 'right') {
-        direction = 'left';
-    } else if (e.key == 'ArrowUp' && direction != 'down') {
-        direction = 'up';
-    } else if (e.key == 'ArrowRight' && direction != 'left') {
-        direction = 'right';
-    } else if (e.key == 'ArrowDown' && direction != 'up') {
-        direction = 'down';
+    if (steps == true) {
+        if (e.key == 'ArrowLeft' && direction != 'right') {
+            direction = 'left';
+            steps = false;  
+        } else if (e.key == 'ArrowUp' && direction != 'down') {
+            direction = 'up';
+            steps = false;  
+        } else if (e.key == 'ArrowRight' && direction != 'left') {
+            direction = 'right';
+            steps = false;  
+        } else if (e.key == 'ArrowDown' && direction != 'up') {
+            direction = 'down';
+            steps = false;  
+        }
     }
 })
