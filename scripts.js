941ee84b6179d3a354fd55c9ec270245aa2f4278
Maksim Basov <maksimbasov1992@mail.ru>
2021-10-22 11:16:33 +0300

    feat: snake move in all directions

--- scripts.js
+++ scripts.js
@@ -35,9 +35,9 @@ console.log(coordinates)
 console.log(coordinates[0])
 console.log(coordinates[1])
 let snakeBody =
-    [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'), 
-    document.querySelector('[posX = "' + (coordinates[0]-1) + '"][posY = "' + coordinates[1] + '"]'), 
-    document.querySelector('[posX = "' + (coordinates[0]-2) + '"][posY = "' + coordinates[1] + '"]')];
+    [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'),
+    document.querySelector('[posX = "' + (coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'),
+    document.querySelector('[posX = "' + (coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')];
 
 
 for (let i = 0; i < snakeBody.length; i++) {
@@ -45,42 +45,63 @@ for (let i = 0; i < snakeBody.length; i++) {
 }
 
 snakeBody[0].classList.add('snake__head')
-    
+
 let mouse;
 
 function createMouse() {
     function generateMouse() {
         let posX = Math.round(Math.random() * (10 - 3) + 3);
         let posY = Math.round(Math.random() * (10 - 1) + 1);
-    return [posX, posY];
+        return [posX, posY];
     }
     let mouseCoordinates = generateMouse();
     console.log(mouseCoordinates);
     mouse = document.querySelector('[posX = "' + mouseCoordinates[0] + '"][posY = "' + mouseCoordinates[1] + '"]');
-    
-    while(mouse.classList.contains('snakeBody')) {
+
+    while (mouse.classList.contains('snakeBody')) {
         let mouseCoordinates = generateMouse();
         mouse = document.querySelector('[posX = "' + mouseCoordinates[0] + '"][posY = "' + mouseCoordinates[1] + '"]');
     }
-    
+
     mouse.classList.add('mouse');
 }
 
 createMouse();
 
+let direction = 'right';
 
 function move() {
     snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
     snakeBody[0].classList.remove('snake__head');
-    snakeBody[snakeBody.length-1].classList.remove('snake__body');
+    snakeBody[snakeBody.length - 1].classList.remove('snake__body');
     snakeBody.pop();
 
-    if (snakeCoordinates[0] < 10) {
-        snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0]+1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
-    } else {
-        snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'))
+    if (direction == "right") {
+        if (snakeCoordinates[0] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+         } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'))
+        }
+    } else if (direction == "left") {
+        if (snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+         } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'))
+        }
+    } else if (direction == "up") {
+        if (snakeCoordinates[0] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1]+1) + '"]'));
+         } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "1"]'));
+        }
+    } else if (direction == "down") {
+        if (snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (snakeCoordinates[1]-1) + '"]'));
+         } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "10"]'));
+        }
     }
-    
+
     snakeBody[0].classList.add('snake__head');
     for (let i = 0; i < snakeBody.length; i++) {
         snakeBody[i].classList.add('snake__body');
@@ -88,3 +109,15 @@ function move() {
 }
 
 let interval = setInterval(move, 300);
+
+window.addEventListener('keydown', function (e) {
+    if (e.key == 'ArrowLeft' && direction != 'right') {
+        direction = 'left';
+    } else if (e.key == 'ArrowUp' && direction != 'down') {
+        direction = 'up';
+    } else if (e.key == 'ArrowRight' && direction != 'left') {
+        direction = 'right';
+    } else if (e.key == 'ArrowDown' && direction != 'up') {
+        direction = 'down';
+    }
+})
